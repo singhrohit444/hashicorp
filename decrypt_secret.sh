2a89fad5c1be41e71df32e3541e3579feef973bc
singhrohit444 <34085476+singhrohit444@users.noreply.github.com>
2023-09-13 21:36:45 +0200

    Update decrypt_secret.sh

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -13,6 +13,6 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/my_secret.txt my_secret.json.gpg
+--output $HOME/secrets/my_secret.json my_secret.json.gpg
 
 

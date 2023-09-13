c64d58e6fb48a57d5750786d4a08ea7f7ff012d1
Rohit Singh <singhrohit444@gmail.com>
2023-09-13 21:33:42 +0200

    reverting to previous version

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #variables
-input_encrypted_file = $1
-output_decrypted_file = $(echo "$input_encrypted_file" | awk -F. '{print $1}')
+#input_encrypted_file = $1
+#output_decrypted_file = $(echo "$input_encrypted_file" | awk -F. '{print $1}')
 
-if [ -z "$input_encrypted_file" ]; then exit fi
-if [ -z "$output_decrypted_file" ]; then exit fi
+#if [ -z "$input_encrypted_file" ]; then exit fi
+#if [ -z "$output_decrypted_file" ]; then exit fi
 
 
 # Decrypt the file
@@ -13,5 +13,6 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/$output_decrypted_file.txt $input_encrypted_file
+--output $HOME/secrets/my_secret.txt my_secret.json.gpg
+
 

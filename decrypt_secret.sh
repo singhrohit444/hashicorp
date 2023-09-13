268e6578f1dfb131e3f0447234bc23f819073273
Rohit Singh <singhrohit444@gmail.com>
2023-09-13 21:16:51 +0200

    parameterized workflow

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+#variables
+input_encrypted_file = $1
+output_decrypted_file = $(echo "$input_encrypted_file" | awk -F. '{print $1}')
+
+if [ -z "$input_encrypted_file" ]; then exit fi
+if [ -z "$output_decrypted_file" ]; then exit fi
+
+
 # Decrypt the file
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
+--output $HOME/secrets/$output_decrypted_file.txt $input_encrypted_file
 
